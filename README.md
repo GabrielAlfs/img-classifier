ac12a307968a8b363f367ea398bb8ed5d45744c6
Gabriel Alves <l.luis.gabriel@outlook.com>
2019-08-26 18:06:16 -0300

    feat(vue): vue pre-made template

---
+++ README.md
@@ -0,0 +1,9 @@
+# Image Classifier
+---
+A simple image classifier using machine learning and Vue.js
+
+### How to setup
+```shell
+> yarn
+> yarn serve
+```
---
+++ src/views/Home.vue
@@ -0,0 +1,86 @@
+<template>
+  <v-card
+    max-width="80%"
+    class="mx-auto"
+  >
+    <v-card-title>A Simple Image Classifier</v-card-title>
+    <v-card-text
+      class="black--text"
+    >
+      <v-file-input
+        v-model="imgFile"
+        label="Select image"
+        filled
+        prepend-icon="mdi-camera"
+        @change="onImageChange"
+      ></v-file-input>
+      <v-expand-transition>
+        <div
+          v-if="selectedImage"
+        >
+          <PreviewImage :imgUrl="imgURL"/>
+          <v-row align="center" justify="center">
+            <v-btn
+              type="submit"
+              color="primary"
+              class="mt-5"
+              outlined
+              @click="predict"
+              :loading="loading"
+            >
+              Predict
+            </v-btn>
+          </v-row>
+        </div>
+      </v-expand-transition>
+      <v-expand-transition>
+        <Results
+          v-if="results"
+          :results="results"
+        />
+      </v-expand-transition>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+import axios from 'axios';
+
+/* eslint-disable import/no-unresolved */
+import Results from '@/components/Results.vue';
+import PreviewImage from '@/components/PreviewImage.vue';
+
+export default {
+  data: () => ({
+    imgFile: null,
+    imgURL: null,
+    selectedImage: false,
+    results: null,
+    loading: false,
+  }),
+  methods: {
+    predict() {
+      const formData = new FormData();
+      formData.append('image_file', this.imgFile);
+      this.loading = true;
+      axios.post('https://imgclass1.herokuapp.com/predict', formData).then((res) => {
+        this.loading = false;
+        this.results = res.data;
+      });
+    },
+    onImageChange() {
+      this.results = null;
+      if (this.imgFile !== null) {
+        this.selectedImage = true;
+        this.imgURL = URL.createObjectURL(this.imgFile);
+      } else {
+        this.selectedImage = false;
+      }
+    },
+  },
+  components: {
+    Results,
+    PreviewImage,
+  },
+};
+</script>
